8c41589e9555168162491975fdeca77c7f645bbf
maorsmilga-sketch <maorsmilga@gmail.com>
2025-11-05 09:36:56 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+window.APP_CONFIG = {
+  API_URL: 'https://script.google.com/macros/s/AKfycbyH-w6LquUMiXSZG7tglMUdNqR6Io0HGwdXuT2N75D_apxLfr1KLBhHRAODnFIPrwSKHQ/exec', // ה-URL שקיבלת מה-Deploy (../exec)
+  SECRET:  '112233'                        // אותו סוד כמו ב-Apps Script
+};
