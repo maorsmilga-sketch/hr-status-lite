f287db2b58cc9d7869efb88b24f42a42c07546bc
maorsmilga-sketch <maorsmilga@gmail.com>
2025-11-07 11:19:36 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,11 +2,3 @@ window.APP_CONFIG = {
   API_URL: 'https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLhdvcyI-5KBhP7HTxbwWndrYj6s9-ZMJbg5ZEV18_o7h_OtjMrWx5rocqaVjSROL0jRKIjCiSGfSoFWO1hPeTCZ8MWYVqHDlqiJqgbTescvvzA-Pv8-JstTTxzjHgJPPlS9uFQERbv6KLo5bjn-UzH74wepWZqe1cqEynnRcN8j-zsOwQMySWaGjD47giJH8XkK4Og7M4JxAraCPlaoG69BIU344hj0AwLZXAJIEXl1XqCgP7HC8gicciIxuth_mrAgS3xfzBm9UebRVI3ucU3jc2Ly_ldd2PUndVCh&lib=M36k-H6CptZKNQ3ai86TgebUoOrlwA9TA',
   SECRET: '112233'
 };
-
-
-
-
-
-
-
-
