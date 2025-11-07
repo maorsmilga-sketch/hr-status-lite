e552c84e8b9d63cb5fd8429b0a0af344913ea1b1
maorsmilga-sketch <maorsmilga@gmail.com>
2025-11-07 11:31:00 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 window.APP_CONFIG = {
-  API_URL: 'https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLgIFQx4GG5pQbKz3oZH5NuY_NefrHGTETdd-JDFnrEWStrkZF3fKavB72Zd-Pd2TjSMTwQUZBoAbCfF_Yl-xGBYVpfTOir8B3fQnJ4BQY8hHFs5zj3_Lt1Bd2Vlzwu51PyyvRIIuglUTQK2oZzLVXK5t-pKcsAmUNK3xDKzbPlUsMoezwya8XJz8rUvh-U3kXH0SRDox3SSsvfbD6PHw9D3_BvKwr4koK7kDwlKWNRR5SD567v2vpeYJ9Upb2-F17YGPEhIKzUSIPK1jKeXujfHji6EUKZ6Piy39zqn&lib=M36k-H6CptZKNQ3ai86TgebUoOrlwA9TA',
-  SECRET: '112233'
+  API_URL: 'https://script.google.com/macros/s/AKfycbw6tWwoj0KQAo25UObUc9-tNjCDlWAuVMn6NwwX4wqJwgx78xwvxrlN1FJAhERrIt07/exec',
+  SECRET: 'maor-777'
 };
+
 
